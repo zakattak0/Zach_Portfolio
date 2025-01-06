feaf3203a623f8abf5a4c4cdf356b89521ac7596
Zach Bernas <zbernas757@gmail.com>
2025-01-06 14:28:58 -0500

    Deployment

--- src/content_option.js
+++ src/content_option.js
@@ -1,39 +1,35 @@
-const logotext = "JOHN";
+const logotext = "Zach";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Zachary Bernas",
+    description: "My name is Zachary Bernas",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Zach Bernas",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "I make cool websites",
+        third: "I work with a lot of data",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Aspiring software engineer & data scientist with hands-on experience in data tools like Power BI and Power Apps, while also having a proficiency in Python, Java, HTML, CSS, Javascript, and many other programming languages, seeking to leverage technical skills in data analysis and application development.",
+    your_img_url: "https://media.licdn.com/dms/image/v2/D5603AQG-EWLlwTtX2g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727713748624?e=1741824000&v=beta&t=Jkwrne_uHub6001MjjoIEuNIx_PZTl581ePZRY8pgfQ",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "A Bit About Myself",
+    aboutme: "I'm a current student at the University of Virginia, working on growing my programming and data science skills. I'm currently seeking an internship at a professional level to contribute to a sophisticated team of like-minded engineers and professionals.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Intern",
+        where: "Hazen & Sawyer",
+        date: "5/24-8/24",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Front Desk Associate",
+        where: "First Tee of Hampton Roads",
+        date: "10/22-11/23",
     },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+    
 ];
 
 const skills = [{
@@ -41,34 +37,38 @@ const skills = [{
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Java",
         value: 85,
     },
     {
-        name: "Javascript",
-        value: 80,
+        name: "HTML/CSS",
+        value: 75,
     },
     {
         name: "React",
-        value: 60,
+        value: 70,
     },
     {
-        name: "Jquery",
+        name: "Power BI",
         value: 85,
+    },
+    {
+        name: "Power Apps",
+        value: 80,
     },
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Experienced in front-end development using tools such as HTML, CSS, React, Javascript, etc. ",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Back-End Development",
+        description: "Worked on many projects requiring back-end skills like Java, Python, Git, C, etc.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Data Science and Analysis",
+        description: "Proficient in real-world data analytics being familiar with Power BI, Power Apps, Excel, and Microsoft Office",
     },
 ];
 
@@ -136,21 +136,21 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "zbernas757@gmail.com",
+    YOUR_FONE: "(757)270-8164",
+    description: "Please text or email me if interested in my services or skills.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_ged0wki",
+    YOUR_TEMPLATE_ID: "template_crysfhw",
+    YOUR_USER_ID: "0Yt8VvKp1-4HMYy6S",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/zakattak0",
+    //facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/zachary-bernas-667077313/",
+    //twitter: "https://twitter.com",
 };
 export {
     meta,
